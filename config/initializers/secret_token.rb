84c5fee17ac65b8f46bfb405f8c4857241e49820
Greg Lincoln <greg@greglincoln.com>
2012-01-19 21:46:24 -0500

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuneZombie::Application.config.secret_token = '36bb1265438722763820d1dea02982873400d60525673f357a6672a2790653349110f327a4c7dbe7b295db56a9ce05de6b0a1be6c2ca88afaa09e7c17932d710'
