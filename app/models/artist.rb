e8e2643e4a148f5f40af7d990e0e9499cf0473a2
Greg Lincoln <greg@greglincoln.com>
2012-01-20 23:54:25 -0500

    Remove RubyMine cruft, add licensing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,19 @@
+#    This file is part of TuneZombie.
+#    Copyright 2012 Greg Lincoln
+#
+#    TuneZombie is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    TuneZombie is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with TuneZombie.  If not, see <http://www.gnu.org/licenses/>.
+
 class Artist < ActiveRecord::Base
   has_many :tracks
   has_many :composed_tracks, :class_name => "Track"
---
+++ readme.md
@@ -0,0 +1,19 @@
+TuneZombie
+==========
+
+TuneZombie eats your tunes. It will not yet read your runes.  
+It matters not, as we don't care. Reading runes would not be fair.
+
+*Tilverton lives!*
+
+License
+=======
+    TuneZombie is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TuneZombie is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
