a9cc185808c252fb3971d090adcc52b72734bfd0
Greg Lincoln <tetious@gmail.com>
2012-01-23 11:48:39 -0500

    Post aborted backbone cleanup.

--- Gemfile
+++ Gemfile
@@ -19,8 +19,6 @@ end
 
 gem 'jquery-rails'
 
-gem "rails-backbone"
-
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,8 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
+//= require jquery.event.drag
 //= require_tree .
+
