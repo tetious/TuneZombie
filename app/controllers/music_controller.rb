37db3ed73fa726b129625d4544e89b26682493c2
Greg Lincoln <tetious@gmail.com>
2012-01-31 13:05:17 -0500

    Back to base function in the music controller.

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,10 +1,16 @@
 class MusicController < ApplicationController
 
+  respond_to :html
+
   # music/tab/albums/?album_name=foo
   def index
-    @items = Hash[*params[:specs].split('/')]
-    parse_glob 'boo'
-    render text: 'bar'
+
+    #@items = Hash[*params[:specs].split('/')] if !params[:specs].nil?
+    #parse_glob 'boo'
+
+    @albums = Album.all
+
+    respond_with @albums
   end
 
 end
