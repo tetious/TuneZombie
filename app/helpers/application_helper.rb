383139a7f99d37af902ffd965928cb0013bf6a0e
Greg Lincoln <tetious@gmail.com>
2012-02-01 14:19:56 -0500

    Trying out datatables for track list.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def datatable_for(columns, options = {})
+    content_tag :table, id: options[:id] do
+      thead(columns)
+    end
+  end
+
+  def thead(columns)
+    content_tag :thead do
+      content_tag :tr do
+        columns.each do |c|
+          concat(content_tag(:th, c[:name]))
+        end
+      end
+    end
+  end
+
+  #def column(name, value = nil, &block)
+  #  value = name unless value
+  #  @columns << {:name => name, :value => value, :block => block}
+  #end
+
+
 end
