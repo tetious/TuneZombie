25916a81db5ce6bff43176feaab9fcf4e1b2e4e5
Greg Lincoln <tetious@gmail.com>
2012-02-20 12:38:16 -0500

    Add play_count.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120211024112) do
+ActiveRecord::Schema.define(:version => 20120220173727) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -51,10 +51,11 @@ ActiveRecord::Schema.define(:version => 20120211024112) do
 
   create_table "track_plays", :force => true do |t|
     t.datetime "played_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "track_id"
     t.integer  "user_id"
+    t.integer  "played_time"
   end
 
   create_table "tracks", :force => true do |t|
