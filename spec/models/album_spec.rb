2777ba157483d34c2054716957df4ab88d80b8f2
Greg Lincoln <tetious@gmail.com>
2012-02-28 14:51:50 -0500

    Fix album spec to reflect changes to model. (yes, I know I'm bad)

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,19 +2,14 @@ require 'spec_helper'
 
 describe Album do
   describe '.art_url' do
-    it "should build based on album name" do
-      album = FactoryGirl.build(:album, name: 'Tilverton Lives', art_type: 'jpg')
-      album.art_url.should == '/Test/Path/.__TZAlbumArt__/Tilverton_Lives.jpg'
+    it "should build based on album id" do
+      album = FactoryGirl.build(:album, id: 1, name: 'Tilverton Lives', art_type: 'jpg')
+      album.art_url.should == '/Test/Path/.__TZAlbumArt__/1.jpg'
     end
 
-    it "should generate a filesafe path" do
-      album = FactoryGirl.build(:album, name: 'Tilverton:$#@% Lives', art_type: 'png')
-      album.art_url.should == '/Test/Path/.__TZAlbumArt__/Tilverton_Lives.png'
-    end
-
-    it "should exclude extention if art_type is not provided" do
-      album = FactoryGirl.build(:album, name: 'Tilverton Lives')
-      album.art_url.should == '/Test/Path/.__TZAlbumArt__/Tilverton_Lives.'
+    it "should return nil if art_type is not provided" do
+      album = FactoryGirl.build(:album, id: 1, name: 'Tilverton Lives')
+      album.art_url.should be_nil
     end
   end
 
