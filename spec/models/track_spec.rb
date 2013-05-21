f31b7513350dc556587c5d1f1a455efc84a48ec7
Greg Lincoln <tetious@gmail.com>
2013-05-21 15:40:22 -0400

    fix track_spec after factory changes

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -13,20 +13,20 @@ describe Track do
 
     it "should generate a path with a missing artist" do
       album = FactoryGirl.build(:album, name: 'Tilverton Lives')
-      track = FactoryGirl.build(:track, album: album)
+      track = FactoryGirl.build(:track, album: album, artist: nil)
       track.file_path.should == '/Test/Path/__nil__/Tilverton_Lives/test.m4a'
 
     end
 
     it "should generate a path with a missing album" do
       artist = FactoryGirl.build(:artist, name: 'Bob Barker')
-      track = FactoryGirl.build(:track, artist: artist)
+      track = FactoryGirl.build(:track, album: nil, artist: artist)
 
       track.file_path.should == '/Test/Path/Bob_Barker/__nil__/test.m4a'
     end
 
     it "should generate a path with a missing album and artist" do
-      track = FactoryGirl.build(:track)
+      track = FactoryGirl.build(:track, album: nil, artist: nil)
 
       track.file_path.should == '/Test/Path/__nil__/__nil__/test.m4a'
     end
