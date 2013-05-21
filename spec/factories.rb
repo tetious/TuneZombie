c1b55ca0cd28a31a5f4f0685fe852a45c0935f9c
Greg Lincoln <tetious@gmail.com>
2013-05-21 14:12:55 -0400

    add album list request spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,17 @@ FactoryGirl.define do
 
   factory :album do
     name 'Test Album'
+
+    factory :album_with_tracks do
+      ignore do
+        track_count 10
+      end
+
+      after(:create) do |album, evaluator|
+        FactoryGirl.create_list(:track, evaluator.track_count, album: album)
+      end
+    end
+
   end
 
   factory :artist do
@@ -11,6 +22,9 @@ FactoryGirl.define do
   factory :track do
     name 'Test Track'
     filename 'test.m4a'
+    artist
+    album
+    sequence(:number)
   end
 
 end
--- spec/requests/albums_spec.rb
+++ spec/requests/albums_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
-describe "Albums" do
-  describe "GET /albums" do
-    #it "works! (now write some real specs)" do
-    #  # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    #  get albums_path
-    #  response.status.should be(200)
-    #end
+describe 'Albums' do
+  describe 'GET /albums' do
+
+    it 'lists albums' do
+      FactoryGirl.create_list(:album_with_tracks, 5)
+      get albums_path, format: 'json'
+      response.status.should be(200)
+      JSON.parse(response.body).length.should be(5)
+    end
+
   end
 end
